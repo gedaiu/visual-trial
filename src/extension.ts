325f3954991d968316e5632538508a7a44f47b9e
Szabo Bogdan <bogdan.szabo@altom.ro>
2017-08-29 22:33:37 +0300

    show the text editor

--- src/extension.ts
+++ src/extension.ts
@@ -3,14 +3,23 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-import { TrialTestsDataProvider } from './trialTestsDataProvider'
+import { TrialTestsDataProvider, TrialNode, TestLocation } from './trialTestsDataProvider'
+import { Range } from "vscode";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    const trialTests = new TrialTestsDataProvider(vscode.workspace.rootPath);
+    const trialTests = new TrialTestsDataProvider(vscode.workspace.rootPath, context);
 
     vscode.window.registerTreeDataProvider('trialTests', trialTests);
+
+    vscode.commands.registerCommand('goToTestSource', (location: TestLocation) => {
+        vscode.workspace.openTextDocument(location.fileName).then(document => {
+            vscode.window.showTextDocument(document, {
+                selection: new Range(location.line, 0, location.line, 0)
+            });
+        });
+    });
 }
 
 // this method is called when your extension is deactivated
--- src/trialTestsDataProvider.ts
+++ src/trialTestsDataProvider.ts
@@ -1,8 +1,126 @@
 import { ExtensionContext, TreeDataProvider, EventEmitter, TreeItem, Event, window, TreeItemCollapsibleState, Uri, commands, workspace, TextDocumentContentProvider, CancellationToken, ProviderResult, ProcessExecution } from 'vscode';
 import * as ChildProcess from "child_process"
+import * as path from 'path';
+import * as vscode from 'vscode';
 
-export class TrialNode {
-    constructor(public name: string) {
+export interface TrialNode {
+    toTreeItem(): TreeItem;
+    getChildren(): TrialNode[] | Thenable<TrialNode[]>;
+}
+
+export interface TestLocation {
+    fileName: string;
+    line: number;
+}
+
+export class TestCaseTrialNode implements TrialNode {
+    constructor(public data: any, private context: vscode.ExtensionContext) {
+
+    }
+
+    toTreeItem(): TreeItem {
+        return {
+            label: this.data.name,
+            collapsibleState: TreeItemCollapsibleState.None,
+            command: {
+                command: 'goToTestSource',
+                arguments: [ this.data.location ],
+                title: 'Go to the test source'
+            }
+        };
+    }
+
+    getChildren(): TrialNode[] | Thenable<TrialNode[]> {
+        return [];
+    }
+
+}
+
+export class SuiteTrialNode implements TrialNode {
+
+    constructor(public name: string, public childElements: any, private context: vscode.ExtensionContext) {
+
+    }
+
+    toTreeItem(): TreeItem {
+        return {
+            label: this.name,
+            iconPath: {
+                light: this.context.asAbsolutePath(path.join('resources', 'light', 'suite.svg')),
+                dark: this.context.asAbsolutePath(path.join('resources', 'dark', 'suite.svg'))
+            },
+
+            collapsibleState: TreeItemCollapsibleState.Collapsed
+        };
+    }
+
+    getChildren(): TrialNode[] | Thenable<TrialNode[]> {
+        if (Array.isArray(this.childElements)) {
+            return this.childElements.map(a => new TestCaseTrialNode(a, this.context));
+        }
+
+        return Object.keys(this.childElements).map(a => new SuiteTrialNode(a, this.childElements[a], this.context));
+    }
+}
+
+export class TrialRootNode implements TrialNode {
+    testFetcher: Thenable<TrialNode[]>;
+
+    constructor(public name: string, public projectRoot: string, private context: vscode.ExtensionContext) {
+    }
+
+    toTreeItem(): TreeItem {
+        return {
+            label: this.name,
+            iconPath: {
+                light: this.context.asAbsolutePath(path.join('resources', 'light', 'package.svg')),
+                dark: this.context.asAbsolutePath(path.join('resources', 'dark', 'package.svg'))
+            },
+            collapsibleState: TreeItemCollapsibleState.Collapsed
+        };
+    }
+
+    getChildren(): TrialNode[] | Thenable<TrialNode[]> {
+        if (this.testFetcher) {
+            return this.testFetcher;
+        }
+
+        this.testFetcher = new Promise((resolve, reject) => {
+            let options = ["describe"];
+
+            if (this.name.indexOf(":") === 0) {
+                options.push(this.name);
+            }
+
+            const trialProcess = ChildProcess.spawn("trial", options, { cwd: this.projectRoot });
+            let rawDescription = "";
+
+            trialProcess.stdout.on('data', (data) => {
+                rawDescription += data;
+            });
+
+            trialProcess.on('close', (code) => {
+                if (code !== 0) {
+                    reject(`trial process exited with code ${code}`);
+                }
+
+                let description: any;
+
+                try {
+                    description = JSON.parse(rawDescription);
+                } catch (e) {
+                    reject(e);
+                    return;
+                }
+
+                let items: TrialNode[] = Object.keys(description).map(a => new SuiteTrialNode(a, description[a], this.context));
+
+                resolve(items);
+            });
+        });
+
+
+        return this.testFetcher;
     }
 }
 
@@ -13,38 +131,20 @@ export class TrialTestsDataProvider implements TreeDataProvider<TrialNode> {
 
     private rootNodesFetcher: Thenable<TrialNode[]>;
 
-    constructor(public projectRoot: string) {
+    constructor(public projectRoot: string, private context: vscode.ExtensionContext) {
     }
 
     public getTreeItem(element: TrialNode): TreeItem {
-        var item = {
-            label: element.name
-        };
-
-        return item;
-/*
-        return {
-            label: element.name,
-            collapsibleState: element.isFolder ? TreeItemCollapsibleState.Collapsed : void 0,
-            command: element.isFolder ? void 0 : {
-                command: 'openFtpResource',
-                arguments: [element.resource],
-                title: 'Open FTP Resource'
-            },
-            iconPath: {
-                light: element.isFolder ? path.join(__filename, '..', '..', '..', 'resources', 'light', 'folder.svg') : path.join(__filename, '..', '..', '..', 'resources', 'light', 'document.svg'),
-                dark: element.isFolder ? path.join(__filename, '..', '..', '..', 'resources', 'dark', 'folder.svg') : path.join(__filename, '..', '..', '..', 'resources', 'dark', 'document.svg')
-            }
-        };*/
+        return element.toTreeItem();
     }
 
     private rootNodes(): Thenable<TrialNode[]> {
-        if(this.rootNodesFetcher) {
+        if (this.rootNodesFetcher) {
             return this.rootNodesFetcher;
         }
 
         this.rootNodesFetcher = new Promise((resolve, reject) => {
-            const trialProcess = ChildProcess.spawn("trial", [ "subpackages" ], { cwd: this.projectRoot });
+            const trialProcess = ChildProcess.spawn("trial", ["subpackages"], { cwd: this.projectRoot });
             let rawSubpackages = "";
 
             trialProcess.stdout.on('data', (data) => {
@@ -56,9 +156,11 @@ export class TrialTestsDataProvider implements TreeDataProvider<TrialNode> {
                     reject(`trial process exited with code ${code}`);
                 }
 
-                let items = rawSubpackages.trim().split("\n").map(a => new TrialNode(a));
+                let items = rawSubpackages.trim().split("\n").map(a => new TrialRootNode(a, this.projectRoot, this.context));
 
                 resolve(items);
+
+                this.rootNodesFetcher = null;
             });
         });
 
@@ -70,6 +172,6 @@ export class TrialTestsDataProvider implements TreeDataProvider<TrialNode> {
             return this.rootNodes();
         }
 
-        return [];//this.model.getChildren(element);
+        return element.getChildren();
     }
 }
