53f7bf2a53b0c028c67b950de1e6cc0c0d927602
Szabo Bogdan <szabobogdan@yahoo.com>
2017-09-02 13:26:31 +0300

    parse TAP result lines

--- src/tapParser.ts
+++ src/tapParser.ts
@@ -1,21 +1,14 @@
-
-
-export interface TestResult {
-    name: string;
-    suite: string;
-    result: string;
-}
-
 export class TapParser {
-    private eventFunction: any;
+    private eventFunction: (result: TestResult) => void;
 
-    onTestResult(eventFunction) {
+    onTestResult(eventFunction: (result: TestResult) => void) {
         this.eventFunction = eventFunction;
     }
 
     setData(data: string | Buffer) {
         if(typeof data === "string" || data instanceof String) {
             this.setString(<string> data);
+            return;
         }
 
         this.setString(data.toString());
@@ -23,20 +16,60 @@ export class TapParser {
         
     setString(data: string) {
         data.split("\n").forEach((line: string) => {
-            console.log(line);
-
-            if(line.indexOf("ok - ") === 0) {
-                line = line.substr(5);
-                var pieces = line.split(".");
-
-                var testResult = {
-                    name: pieces[pieces.length - 1],
-                    suite: pieces.slice(0, pieces.length - 1).join("."),
-                    result: "ok"
-                };
-
-                this.eventFunction(testResult);
+            if(line.indexOf("ok - ") === 0 || line.indexOf("not ok - ") === 0) {
+                this.eventFunction(new TestResult(line));
             }
         });
     }
 }
+
+export class TestResult {
+    private static resultValues: Array<string> = [ "ok", "not ok" ];
+
+    private _value: string;
+    private _name: string;
+    private _suite: string;
+    private _index: number = NaN;
+
+    constructor(private line: string) {
+        const value = TestResult.resultValues.filter((value) => line.indexOf(value + " ") === 0);
+        if(value.length == 1) {
+            this._value = value[0];
+            line = line.substr(this._value.length + 1); /// we want to remove the space too
+        } else {
+            this._value = "unknown";
+            line = line.substr(line.indexOf(" ")).trim();
+        }
+
+        const firstSpaceIndex = line.indexOf(" ");
+        const firstDotIndex = line.indexOf(".");
+        this._index = parseInt(line.substr(0, firstSpaceIndex));
+        
+        if(firstDotIndex > firstSpaceIndex) {
+            line = line.substr(firstSpaceIndex).trim();
+        }
+
+        var pieces = line.split(".");
+        this._name = pieces.reverse()[0];
+
+        pieces.reverse().pop();
+        this._suite = pieces.join(".");
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
+    get suite(): string {
+        return this._suite;
+    }
+
+    get value(): string {
+        return this._value;
+    }
+    
+    get index(): number {
+        return this._index;
+    }
+}
+
--- test/tap.test.ts
+++ test/tap.test.ts
@@ -1,11 +1,12 @@
 // The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
+import * as should from 'should';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
 import * as myExtension from '../src/extension';
-import { TapParser } from "../src/tapParser";
+import { TapParser, TestResult } from "../src/tapParser";
 
 var oneSuccessTest = `Building package trial:lifecycle in /Users/gedaiu/workspace/trial/
 Generate main file: /Users/gedaiu/workspace/trial/generated.d
@@ -26,19 +27,141 @@ TAP version 13
 ok - trial.settings.it should be able to compile the settings code
 `
 
+var oneFailedTest = `Building package trial:lifecycle in /Users/gedaiu/workspace/trial/
+Generate main file: /Users/gedaiu/workspace/trial/generated.d
+Looking for files inside \`trial:lifecycle\`
+Selecting tests conaining \`it should be able to compile the settings code\`.
+Building package trial:lifecycle in /Users/gedaiu/workspace/trial/
+Generating test runner configuration 'trial-lifecycle-test-library' for 'library' (library).
+Performing "unittest" build using dmd for x86_64.
+ddmp 0.0.1-0.dev.2: target for configuration "library" is up to date.
+fluent-asserts:core 0.6.6: target for configuration "library" is up to date.
+fluent-asserts 0.6.6: target for configuration "library" is up to date.
+trial:lifecycle 0.3.1+commit.16.g2f5e957: building configuration "trial-lifecycle-test-library"...
+Linking...
+To force a rebuild of up-to-date targets, run again with --force.
+Running ./trial-lifecycle-test-library
+TAP version 13
+1..1
+not ok - trial.discovery.unit.It should find the line of this test
+# core.exception.RangeError: Range violation
+#
+# --------------------
+# lifecycle/trial/discovery/unit.d:426
+# --------------------
+#    421:
+#    422:       testDiscovery.addModule!(__FILE__, "trial.discovery.unit");
+#    423:
+#    424:       testDiscovery.testCases.keys.should.contain("trial.discovery.unit");
+#    425:
+# >  426:       auto r = testDiscovery.testCases["trial.dwiscovery.unit"].values.filter!(a => a.name == "It should find the line of this test");
+#    427:
+#    428:       r.empty.should.equal(false).because("the location should be present");
+#    429:       r.front.location.fileName.should.endWith("unit.d");
+#    430:       r.front.location.line.should.equal(line - 1);
+#    431: }
+# --------------------
+#
+# Stack trace:
+# -------------------
+# ...
+# 4   0x000000010eb98b22 _d_arrayboundsp at trial-lifecycle-test-library + 110
+# 5   0x000000010eaba3bd void trial.discovery.unit.__unittestL418_93() at trial-lifecycle-test-library + 313
+# ...
+#
+  ---
+  message: 'core.exception.RangeError: Range violation'
+  severity: failure
+  location:
+    fileName: 'lifecycle/trial/discovery/unit.d'
+    line: 426
+`
+
 suite("TAP Protocol", () => {
 
-    test("should find a successful tests", (done) => {
+    test("It should parse a success test result", () => {
+        var result = new TestResult("ok - trial.settings.it should be able to compile the settings code");
+
+        should(result.value).equal("ok");
+        should(result.suite).equal("trial.settings");
+        should(result.name).equal("it should be able to compile the settings code");
+    });
+
+    test("It should parse a failed test result", () => {
+        var result = new TestResult("not ok - trial.settings.it should be able to compile the settings code");
+
+        should(result.value).equal("not ok");
+        should(result.suite).equal("trial.settings");
+        should(result.name).equal("it should be able to compile the settings code");
+    });
+
+    test("It should parse a success test result with the test index", () => {
+        var result = new TestResult("ok 1020 trial.settings.it should be able to compile the settings code");
+
+        should(result.value).equal("ok");
+        should(result.suite).equal("trial.settings");
+        should(result.name).equal("it should be able to compile the settings code");
+        should(result.index).equal(1020);
+    });
+
+    test("It should parse a success test result without the test index", () => {
+        var result = new TestResult("ok trial.settings.it should be able to compile the settings code");
+
+        should(result.value).equal("ok");
+        should(result.suite).equal("trial.settings");
+        should(result.name).equal("it should be able to compile the settings code");
+    });
+
+    test("It should parse a success test result without a suite", () => {
+        var result = new TestResult("ok it should be able to compile the settings code");
+
+        should(result.value).equal("ok");
+        should(result.suite).equal("");
+        should(result.name).equal("it should be able to compile the settings code");
+    });
+
+    test("It should parse a test with an unknown result", () => {
+        var result = new TestResult("okish trial.settings.it should be able to compile the settings code");
+
+        should(result.value).equal("unknown");
+        should(result.suite).equal("trial.settings");
+        should(result.name).equal("it should be able to compile the settings code");
+    });
+
+
+    test("It should parse an empty line", () => {
+        var result = new TestResult("");
+
+        should(result.value).equal("unknown");
+        should(result.suite).equal("");
+        should(result.name).equal("");
+    });
+
+    test("should find a successful test", (done) => {
         var parser = new TapParser();
 
         parser.onTestResult((result) => {
-            assert.equal(result.name, "it should be able to compile the settings code");
-            assert.equal(result.suite, "trial.settings");
-            assert.equal(result.result, "ok");
+            should(result.name).equal("it should be able to compile the settings code");
+            should(result.suite).equal("trial.settings");
+            should(result.value).equal("ok");
 
             done();
         });
 
         parser.setData(oneSuccessTest);
+    });
+
+    test("should find a failed test", (done) => {
+        var parser = new TapParser();
+
+        parser.onTestResult((result) => {
+            should(result.name).equal("It should find the line of this test");
+            should(result.suite).equal("trial.discovery.unit");
+            should(result.value).equal("not ok");
+
+            done();
+        });
+
+        parser.setData(oneFailedTest);
     });
 });
